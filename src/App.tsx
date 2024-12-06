4219037d56e8807690cca577f8c352d1ba0a56e7
Ehteshambutt <ehteshambutt58@gmail.com>
2024-12-06 12:48:08 +0500

    aa

---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+import { Config, Puck, Render } from '@measured/puck';
+import '@measured/puck/puck.css';
+import { config } from 'puck-mui';
+import { useEffect, useState } from 'react';
+import { AppBar, Box, Button, Toolbar } from '@mui/material';
+import * as initialData from './inital-data.json';
+
+const onPublish = (data: any) => {
+  localStorage.setItem('dashboard', JSON.stringify(data));
+};
+
+function App() {
+  const [data, setData] = useState<any>(undefined);
+  const [preview, setPreview] = useState(false);
+
+  useEffect(() => {
+    const data = localStorage.getItem('dashboard');
+    if (data) {
+      setData(JSON.parse(data));
+    } else {
+      setData(initialData);
+    }
+  }, []);
+
+  if (!data) {
+    return null;
+  }
+
+  if (preview) {
+    return (
+      <>
+        <AppBar sx={{ backgroundColor: 'white' }}>
+          <Toolbar>
+            <Box flexGrow={1} />
+            <Button variant='text' onClick={() => setPreview(false)}>
+              Edit
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <Box sx={{ mt: '64px' }}>
+          <Render config={config as Config} data={data} />;
+        </Box>
+      </>
+    );
+  }
+
+  return (
+    <Puck
+      config={config as Config}
+      data={data}
+      onChange={setData}
+      onPublish={onPublish}
+      overrides={{
+        headerActions: (props) => {
+          return (
+            <>
+              <Button onClick={() => setPreview(true)}>Preview</Button>
+              {props.children}
+            </>
+          );
+        }
+      }}
+    />
+  );
+}
+
+export default App;
